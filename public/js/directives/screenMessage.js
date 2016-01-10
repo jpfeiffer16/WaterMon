6f4c9071cb900223919ae9bd01a4c80a6d5357ef
Joe Pfeiffer <jpfeiffer@bluemodus.com>
2016-01-10 14:44:41 +0000

    Some welcome messages

--- public/js/directives/screenMessage.js
+++ public/js/directives/screenMessage.js
@@ -4,10 +4,12 @@ angular.module('app')
 	    restrict: 'E',
 	    replace: true,
 	    link: function(scope, element, attrs) {
-	      var message = attrs['message'];
-	      var duration = attrs['duration'];
-	      var explanation = attrs['explanation'];
-	      $('body').append('<div class="screen-message"><h2>' + message + '</h2><p>' + explanation + '</p></div>').hide().fadeIn(1500);
+	      var message = attrs['message'],
+	        duration = attrs['duration'],
+	        explanation = attrs['explanation'];
+	      $('body').append('<div class="screen-message"><h2>' + message + '</h2><p>' + explanation + '</p></div>')
+	        .hide()
+	        .fadeIn(1500);
         if (duration != undefined) {
           setTimeout(function() {
             $('.screen-message').fadeOut(1000).remove();
