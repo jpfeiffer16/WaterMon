eaeef2c7d13831888f2789b9ecb429b581bfd2b1
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-01-28 06:43:17 -0700

    Fixing formatting

--- app/controllers/getLevel.js
+++ app/controllers/getLevel.js
@@ -1,23 +1,23 @@
 var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  SensorDataReader = require("../modules/sensorDataReader")
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    SensorDataReader = require("../modules/sensorDataReader");
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.post('/getlevel', function (req, res, next) {
-	//TODO: This is just test data. Will need to write a new module for interfacing
-	//with the sensor and drop in a call to it here.
-	SensorDataReader.getLevel(function(err, result) {
-	  if (err) {
-	    res.status(500).send(err);
-	    return;
-	  }
-	  res.send({
-	    result: result
-	  });
-	});
-  
+  //TODO: This is just test data. Will need to write a new module for interfacing
+  //with the sensor and drop in a call to it here.
+  SensorDataReader.getLevel(function(err, result) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.send({
+      result: result
+    });
+  });
+
 });
